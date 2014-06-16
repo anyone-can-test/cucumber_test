f5e4799edec9108e2280217ca9ead3efeb3f047a
anyone-can-test <anyone.can.test@gmail.com>
2014-06-17 01:41:15 +0900

    page 122

--- cal/features/step_definitions/steps.rb
+++ cal/features/step_definitions/steps.rb
@@ -3,17 +3,22 @@ CAPTURE_A_NUMBER = Transform /^\d+$/ do |number|
 	number.to_i
 end
 
-Given /^I have deposited \$(#{CAPTURE_A_NUMBER}) in my account$/ do |amount|
-	my_account = Account.new
-	my_account.deposit(amount)
-
-	# RSpec assertion
-	my_account.balance.should eq(amount),
-		"Expected the balance to be #{amount} but it was #{my_account.balance}"
+CAPTURE_CASH_AMOUNT = Transform /^\$(\d+)$/ do |digits|
+    digits.to_i
 end
 
-When /^I request \$(\d+)$/ do |arg1|
-    pending # express the regexp above with the code you wish you had
+Given /^I have deposited \$(#{CAPTURE_A_NUMBER}) in my account$/ do |amount|
+	@my_account = Account.new
+	@my_account.deposit(amount)
+
+	# RSpec assertion
+	@my_account.balance.should eq(amount),
+		"Expected the balance to be #{amount} but it was #{@my_account.balance}"
+end
+
+When /^I withdraw (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
+    teller = Teller.new
+    teller.withdraw_from(@my_account, amount)
 end
 
 Then /^\$(\d+) should be dispensed$/ do |arg1|
@@ -30,3 +35,18 @@ class Account
 		@balance
 	end
 end
+
+
+class Teller
+    def withdraw_from(account, amount)
+    end
+end
+
+
+module KnowsMyAccount
+    def my_account
+        @my_account ||= Account.new
+    end
+end
+
+World(KnowsMyAccount)
